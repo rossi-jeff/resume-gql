90dc0b88cdd12ddfff08802af828e94c568ff701
Jeff Rossi <jrossi@intouchgps.com>
2023-01-31 08:39:49 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as config from 'config';
 async function bootstrap() {
   const port = config.get('server.port');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
   console.log(`starting server on port: ${port}`);
 }
