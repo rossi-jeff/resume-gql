3104ff57162ac0499bccde8aa42d3afcae1e3ccd
Jeff Rossi <jrossi@intouchgps.com>
2023-01-31 09:56:29 -0500

    config changes until it worked

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
+const corsOptions = {
+  origin: true,
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: 'Content-Type, Accept',
+  credentials: true,
+};
+
 async function bootstrap() {
   const port = config.get('server.port');
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   await app.listen(port);
   console.log(`starting server on port: ${port}`);
 }
